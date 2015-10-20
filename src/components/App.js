00b7d1e2bded497a6c03a3f0beee3e95b3da7407
Francisco De La Cruz <helloquico@gmail.com>
2015-10-20 11:30:57 -0400

    PageList Component - Create Pages

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {RouteHandler} from 'react-router';
 
 import Login from './Login';
+import PageList from './PageList';
 
 export default class App extends React.Component {
 	state = { user: USER } // class property (state) instead of getInitialState, ES7 experimental feature
@@ -16,7 +17,7 @@ export default class App extends React.Component {
 					
 					<Login user={this.state.user} setUser={this.setUser} />
 
-					PageList
+					<PageList user={this.state.user} />
 				</div>
 				<div className='nine columns'>
 					<RouteHandler user={this.state.user} />
