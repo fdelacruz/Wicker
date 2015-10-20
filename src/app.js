51e238212460b4576fa6c23e9a3e892d086712d9
Francisco De La Cruz <helloquico@gmail.com>
2015-10-19 23:20:43 -0400

    Router Setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-var app = 'app';
+/* jshint esnext: true */
+
+import React from 'react';
+import Router from 'react-router';
+import App from './components/App';
+
+const {Route} = Router; // ES6 Destructuring
+
+var routes = <Route handler={App}>
+</Route>;
+
+Router.run(routes, Router.HistoryLocation, Root => // ES6 Arrow Function
+		React.render(<Root />, document.getElementById('app')));
