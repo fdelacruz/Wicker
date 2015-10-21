118a850365604735258fa8203c6ec77db342850b
Francisco De La Cruz <helloquico@gmail.com>
2015-10-20 20:36:10 -0400

    Page Component

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@
 import React from 'react';
 import Router from 'react-router';
 import App from './components/App';
+import Page from './components/Page';
 
 const {Route} = Router; // ES6 Destructuring
 
 var routes = <Route handler={App}>
-	<Route name='page' path='/page/:id' handler={App} />
+	<Route name='page' path='/page/:id' handler={Page} />
 </Route>;
 
 Router.run(routes, Router.HistoryLocation, Root => // ES6 Arrow Function
